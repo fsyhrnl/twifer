66332ec5a12a86cc98ba7ddc5884ea074bc2c97b
Ferry Syahrinal <30984368+ferrysyahrinal@users.noreply.github.com>
2022-04-09 07:33:49 +0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'src/Twifer.php';
+
+$conn = new Twifer('CONSUMER_KEY', 'CONSUMER_SECRET', 'OAUTH_TOKEN', 'OAUTH_TOKEN_SECRET');
+
+// Upload media
+$filename = 'profile.jpg';
+$parameter = ['media' => $filename];
+$req = $conn->request('POST', 'media/upload', $parameter);
+print_r($req);
+
+// Post tweet
+$parameter = ['status' => 'Hi World'];
+$req = $conn->request('POST', 'statuses/update', $parameter);
+print_r($req);
+
+// Get user_timeline
+$parameter = ['screen_name' => 'senggolbaok', 'count' => '2'];
+$req = $conn->request('GET', 'statuses/user_timeline', $parameter);
+print_r($req);
