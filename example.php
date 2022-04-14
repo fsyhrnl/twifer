9c6e6fb15f10ffb65b5843eac83637a29dd96b9d
Anjing <ferrysyahrinal2@gmail.com>
2022-04-15 02:18:23 +0700

    new update, result will show as array without using 'json_decode'

--- example.php
+++ example.php
@@ -16,36 +16,31 @@ $conn = new API('CONSUMER_KEY', 'CONSUMER_SECRET', 'OAUTH_TOKEN', 'OAUTH_TOKEN_S
 $filename = 'profile.jpg';
 $parameter = ['media' => $filename];
 $req = $conn->request('POST', 'media/upload', $parameter);
-$req = json_decode($req, true);
 print_r($req);
 
 // Post tweet
 $parameter = ['status' => 'Hi World'];
 $req = $conn->request('POST', 'statuses/update', $parameter);
-$req = json_decode($req, true);
+
 print_r($req);
 
 // Delete tweet
 $id = '1512864814338506753'; //id tweet
 $req = $conn->request('POST', 'statuses/destroy/' . $id);
-$req = json_decode($req, true);
 print_r($req);
 
 // Post retweet
 $id = '1511820056430538755'; //id tweet
 $req = $conn->request('POST', 'statuses/retweet/' . $id);
-$req = json_decode($req, true);
 print_r($req);
 
 // Get user_timeline
 $parameter = ['screen_name' => 'senggolbaok', 'count' => '2'];
 $req = $conn->request('GET', 'statuses/user_timeline', $parameter);
-$req = json_decode($req, true);
 print_r($req);
 
 // Get direct message
 $req = $conn->request('GET', 'direct_messages/events/list');
-$req = json_decode($req, true);
 print_r($req);
 
 // Fetch image direct message
@@ -57,17 +52,14 @@ file_put_contents('saveImage.jpg', $req);
 // Lookup user by username
 $parameter = ['screen_name' => 'senggolbaok'];
 $req = $conn->request('GET', 'users/lookup', $parameter);
-$req = json_decode($req, true);
 print_r($req);
 
 // Lookup user by id
 $parameter = ['user_id' => '965702083'];
 $req = $conn->request('GET', 'users/lookup', $parameter);
-$req = json_decode($req, true);
 print_r($req);
 
 // Follow user
 $parameter = ['screen_name' => 'senggolbaok'];
 $req = $conn->request('POST', 'friendships/create', $parameter);
-$req = json_decode($req, true);
 print_r($req);
